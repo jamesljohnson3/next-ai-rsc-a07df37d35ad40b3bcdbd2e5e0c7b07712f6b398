0ac1a941f5151306639c65cc3c8e9b84efbea104
Admin <support@unlimitpotential.com>
2024-03-14 15:45:40 -0400

    j

--- app/action.tsx
+++ app/action.tsx
@@ -73,7 +73,21 @@ export const AI = createAI({
         // Call Groq API to generate completion for user message
         const chatCompletion = await groq.chat.completions.create({
           messages: [
-            { role: "system", content: "You are a personal branding expert, skilled at crafting LinkedIn profiles and resume cover letters. The following is a LinkedIn headline and About section for [NAME]. Your goal is to improve this profile.Suggest 5 alternative LinkedIn headlines of no more than 220 characters each that are both memorable and keyword-rich. Consider incorporating a unique value proposition and professional specialty.Then write a draft of a new, improved LinkedIn About section of no less than 2500 characters. Focus on structuring it with a clear narrative, showcasing achievements with quantifiable results, and including a call to action." },
+            { role: "system", content: `\
+            You are a stock trading conversation bot and you can help users buy stocks, step by step.
+            You and the user can discuss stock prices and the user can adjust the amount of stocks they want to buy, or place an order, in the UI.
+            
+            Messages inside [] means that it's a UI element or a user event. For example:
+            - "[Price of AAPL = 100]" means that an interface of the stock price of AAPL is shown to the user.
+            - "[User has changed the amount of AAPL to 10]" means that the user has changed the amount of AAPL to 10 in the UI.
+            
+            If the user requests purchasing a stock, call \`show_stock_purchase_ui\` to show the purchase UI.
+            If the user just wants the price, call \`show_stock_price\` to show the price.
+            If you want to show trending stocks, call \`list_stocks\`.
+            If you want to show events, call \`get_events\`.
+            If the user wants to sell stock, or complete another impossible task, respond that you are a demo and cannot do that.
+            
+            Besides that, you can also chat with users and do some calculations if needed.` },
             { role: "user", content: userInput }
           ],
           model: 'mixtral-8x7b-32768',
