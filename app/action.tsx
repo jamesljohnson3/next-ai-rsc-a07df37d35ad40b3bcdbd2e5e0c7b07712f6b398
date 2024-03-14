930fb6e5489496bcdc88ba6a7d260af4dd879103
Admin <support@unlimitpotential.com>
2024-03-14 15:43:58 -0400

    h

--- app/action.tsx
+++ app/action.tsx
@@ -73,7 +73,7 @@ export const AI = createAI({
         // Call Groq API to generate completion for user message
         const chatCompletion = await groq.chat.completions.create({
           messages: [
-            { role: "system", content: "You are a digital artist conversation bot and you can help users see helpful colors or styles, in the UI." },
+            { role: "system", content: "You are a personal branding expert, skilled at crafting LinkedIn profiles and resume cover letters. The following is a LinkedIn headline and About section for [NAME]. Your goal is to improve this profile.Suggest 5 alternative LinkedIn headlines of no more than 220 characters each that are both memorable and keyword-rich. Consider incorporating a unique value proposition and professional specialty.Then write a draft of a new, improved LinkedIn About section of no less than 2500 characters. Focus on structuring it with a clear narrative, showcasing achievements with quantifiable results, and including a call to action." },
             { role: "user", content: userInput }
           ],
           model: 'mixtral-8x7b-32768',
--- app/test-chat/action2.tsx
+++ app/test-chat/action2.tsx
@@ -8,15 +8,13 @@ import { OpenAIStream, experimental_StreamingReactResponse, Message } from 'ai';
 
 const groq = new Groq()
 
-export async function handler({ messages }: { messages: string }) {
+export async function handler({ messages }: { messages: Message[] }) {
   // Request the OpenAI API for the response based on the prompt
   const response = await groq.chat.completions.create({
     model: 'mixtral-8x7b-32768',
     stream: true,
-    messages: [
-      { role: "system", content: "You are a personal branding expert, skilled at crafting LinkedIn profiles and resume cover letters. The following is a LinkedIn headline and About section for [NAME]. Your goal is to improve this profile.Suggest 5 alternative LinkedIn headlines of no more than 220 characters each that are both memorable and keyword-rich. Consider incorporating a unique value proposition and professional specialty.Then write a draft of a new, improved LinkedIn About section of no less than 2500 characters. Focus on structuring it with a clear narrative, showcasing achievements with quantifiable results, and including a call to action." },
-      { role: "user", content: messages }
-    ],  });
+    messages: messages as any,
+  });
  
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
