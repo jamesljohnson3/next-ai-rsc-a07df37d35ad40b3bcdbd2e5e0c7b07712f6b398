3dad8431783252e0e4099863b20930bd1b41bec0
Admin <support@unlimitpotential.com>
2024-03-14 01:31:15 -0400

    j

--- app/test-chat/action2.tsx
+++ app/test-chat/action2.tsx
@@ -3,6 +3,8 @@ import Groq from 'groq-sdk'
 
 
 import { OpenAIStream, experimental_StreamingReactResponse, Message } from 'ai';
+import { createAI, getMutableAIState, render } from "ai/rsc";
+import { z } from "zod";
  
 
 
@@ -33,3 +35,41 @@ export async function handler({ messages }: { messages: Message[] }) {
     },
   });
 }
+
+async function submitUserMessage(content: string) {
+  'use server';
+
+  const aiState = getMutableAIState<typeof AI>();
+  aiState.update([
+    ...aiState.get(),
+    {
+      role: 'user',
+      content,
+    },
+  ]);}
+
+
+// Define the initial state of the AI. It can be any JSON object.
+const initialAIState: {
+  role: 'user' | 'assistant' | 'system' | 'function';
+  content: string;
+  id?: string;
+  name?: string;
+}[] = [];
+ 
+// The initial UI state that the client will keep track of, which contains the message IDs and their UI nodes.
+const initialUIState: {
+  id: number;
+  display: React.ReactNode;
+}[] = [];
+ 
+// AI is a provider you wrap your application with so you can access AI and UI state in your components.
+export const AI = createAI({
+  actions: {
+    submitUserMessage
+  },
+  // Each state can be any shape of object, but for chat applications
+  // it makes sense to have an array of messages. Or you may prefer something like { id: number, messages: Message[] }
+  initialUIState,
+  initialAIState
+});
