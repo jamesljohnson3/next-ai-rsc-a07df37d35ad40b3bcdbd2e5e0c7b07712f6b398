c4255d7d01555280e61573d2e8731a173ba50647
Admin <support@unlimitpotential.com>
2024-03-14 01:13:36 -0400

    j

---
+++ app/test-chat2/layout.tsx
@@ -0,0 +1,24 @@
+
+
+
+import { AI } from "./action";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+     
+     
+        <AI>
+          {children}
+        </AI>
+    
+    
+    </div>
+  );
+}
