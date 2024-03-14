0334c85ce489b1e937d5c137261000bb0cb591b8
Admin <support@unlimitpotential.com>
2024-03-14 02:06:01 -0400

    h

--- app/action.tsx
+++ app/action.tsx
@@ -25,12 +25,9 @@ import { StockSkeleton } from '@/components/llm-stocks/stock-skeleton';
 import { EventsSkeleton } from '@/components/llm-stocks/events-skeleton';
 import { StocksSkeleton } from '@/components/llm-stocks/stocks-skeleton';
 
-
-import Groq from 'groq-sdk';
-import { OpenAIStream, experimental_StreamingReactResponse, Message } from 'ai';
-
-const groq = new Groq();
-
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || '',
+});
 
 async function confirmPurchase(symbol: string, price: number, amount: number) {
   'use server';
@@ -115,10 +112,34 @@ async function submitUserMessage(content: string) {
     <BotMessage className="items-center">{spinner}</BotMessage>,
   );
 
-  const completion = runOpenAICompletion(groq, {
-    model: 'mixtral-8x7b-32768',
+  const completion = runOpenAICompletion(openai, {
+    model: 'gpt-3.5-turbo',
     stream: true,
-    messages: onmessage,
+    messages: [
+      {
+        role: 'system',
+        content: `\
+You are a stock trading conversation bot and you can help users buy stocks, step by step.
+You and the user can discuss stock prices and the user can adjust the amount of stocks they want to buy, or place an order, in the UI.
+
+Messages inside [] means that it's a UI element or a user event. For example:
+- "[Price of AAPL = 100]" means that an interface of the stock price of AAPL is shown to the user.
+- "[User has changed the amount of AAPL to 10]" means that the user has changed the amount of AAPL to 10 in the UI.
+
+If the user requests purchasing a stock, call \`show_stock_purchase_ui\` to show the purchase UI.
+If the user just wants the price, call \`show_stock_price\` to show the price.
+If you want to show trending stocks, call \`list_stocks\`.
+If you want to show events, call \`get_events\`.
+If the user wants to sell stock, or complete another impossible task, respond that you are a demo and cannot do that.
+
+Besides that, you can also chat with users and do some calculations if needed.`,
+      },
+      ...aiState.get().map((info: any) => ({
+        role: info.role,
+        content: info.content,
+        name: info.name,
+      })),
+    ],
     functions: [
       {
         name: 'show_stock_price',
--- app/test3/page.tsx
+++ app/test3/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from 'react';
 import { useUIState, useActions } from "ai/rsc";
 import { AI } from '../action';
