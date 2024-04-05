8df15fde538f1cb34042f501b75cba72d4de489b
Admin <support@unlimitpotential.com>
2024-04-04 21:14:57 -0400

    s

---
+++ app/test-chat/layout.tsx
@@ -0,0 +1,22 @@
+ import './globals.css';
+
+import { AI } from '../action';
+ 
+export default function RootLayout({
+  children,
+ 
+}: Readonly<{
+  children: React.ReactNode;
+  authModal: React.ReactNode
+
+}>) {
+  return ( 
+        <AI>
+           {children}
+             
+        </AI>
+      
+  );
+}
+
+export const runtime = 'edge';
